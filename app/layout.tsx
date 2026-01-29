511643844371f850444fa8fb6005b013eab06dda
Davide Rubinetti <davide.ruby@gmail.com>
2026-01-29 21:11:12 +0100

    Clean up

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,9 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 
 export const metadata = {
-  metadataBase: new URL('https://postgres-starter.vercel.app'),
-  title: 'Postgres Demo',
-  description:
-    'A simple Next.js app with a Postgres database',
+  // metadataBase: new URL('https://postgres-starter.vercel.app'),
+  title: 'Share Tech Up',
+  description: '',
 }
 
 const inter = Inter({
